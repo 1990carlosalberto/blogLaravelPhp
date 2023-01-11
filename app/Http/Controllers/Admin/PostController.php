27c66ac1ff36139b4b5bdb1df897eda60211d346
Carlos Alberto <admcas02@gmail.com>
2023-01-11 11:01:31 -0300

    Melhorando PostController

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -11,6 +11,16 @@ use App\Models\User;
 
 class PostController extends Controller
 {
+    /**
+     * @var Post
+    */
+    private $post;
+
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
     public function create()
     {
         return view('posts.create');
@@ -19,45 +29,88 @@ class PostController extends Controller
     public function store(Request $request)
     {
         $data = $request->all();
-        $data['is_active']  = true;
+        try{
+            $data['is_active'] = true;
 
-        $user = User::find(1);
+                $user = User::find(1);
+                    $user->posts()->create($data);
 
-        //Continuamos a salvar com mass assignment mas por meio do usuário
-        dd($user->posts()->create($data));
+                    flash('Postagem inserida com sucesso!')->success();
+                    return redirect()->route('posts.index');
+        } catch (\Exception $e) {
+                $message = 'Erro ao remover categoria!';
+
+                if(env('APP_DEBUG')) {
+                    $message = $e->getMessage();
+                }
+
+            flash($message)->warning();
+            return redirect()->back();
+        }
+}
 
 
-    }
+
 
     public function index()
     {
-        $posts = Post::paginate(15);
+        $posts = $this->post->paginate(15);
+
         return view('posts.index', compact('posts'));
     }
 
-    public function show ($post)
+    public function show (Post $post)
     {
-        $post = Post::findOrFail ($post);
         return view('posts.edit', compact('post'));
     }
 
-    public function update ($post, Request $request)
+    public function update (Post $post, Request $request)
     {
-        //atualizando com mass assignmente
+
         $data = $request->all();
 
-        $post = Post::findOrFail($post);
+            try{
+                    $post->update($data);
 
-        dd($post->update($data));
+                flash('Postagem atualizada com sucesso !')->success();
+                return redirect()->route('posts.show',['post'=>$post->id]);
+
+        } catch (\Exception $e) {
+                    $message = 'Erro ao remover categoria!';
+
+                    if(env('APP_DEBUG')) {
+                            $message = $e->getMessage();
+                    }
+
+                 flash($message)->warning();
+                 return redirect()->back();
+        }
     }
 
-    public function destroy ($post)
+
+
+    public function destroy (Post $post)
     {
-        $post = Post::findOrFail($post);
+        try {
 
-        dd($post->delete());
+                $post->delete($post);
+
+                flash('Postagem removida com sucesso!')->success();
+                return redirect()->route('posts.index');
+
+        } catch (\Exception $e) {
+                $message = 'Erro ao remover categoria!';
+
+                if(env('APP_DEBUG')) {
+                        $message = $e->getMessage();
+                }
+
+                flash($message)->warning();
+                return redirect()->back();
+        }
     }
-
-
-
 }
+
+
+
+
